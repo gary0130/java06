8a4554f70477a6cfc182b2c43a38cf35a7a3c16a
gary0130 <gary880130@gmail.com>
2016-08-25 00:11:47 +0800

    添加解密

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,53 @@
 import java.util.*;
 
+import android.R.integer;
+import android.R.string;
+
 public class Main
 {
 	static Scanner scn=new Scanner(System.in);
 	static int x=1;
 	//簡單加密系統
 	public static void main(String[] args)
-	{
+	{	
+		info();
 		while(1==1){
-			info();
+			
 		//help();
 		encode();
 			}
 		
 		
 	}
+	 
 	public static void encode()
 	{
 		//轉換成整數型
 		System.out.println("請輸入要加密的文字");
-		String s1=scn.nextLine(),s2="";
+		String s1=scn.nextLine(),s2="",s3="";
 		char[] ca1=new char[s1.length()];
+		char[] decode=new char[s1.length()];
 		ca1=s1.toCharArray();
 		System.out.println();
 		
 		//數字補零
-		for(int i :ca1)
+		for(int i=0;i<s1.length();i++)
 		{
-			String j=String.valueOf(i);
+			//加密
+			decode[i]=(char) ((int)(ca1[i])-7);//7是偏移量
+			s3=s3+decode[i];
+		
+			String j=String.valueOf((int)ca1[i]);
+			
 			if(j.length()==2){j="0"+j;}
 			s2=s2+String.valueOf(j);
 		}
 		
 		//打印
 		System.out.println(s2);
+		System.out.println(s3);
+		
+		
 		
 	}
 	static void info()
